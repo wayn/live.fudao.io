781023cf0dd1efc47e0e3630a286e2d12f9c014b
Wayn Liu <wayn.info@gmail.com>
2018-12-12 19:55:30 +0800

    no message

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const URL_QINGTAOKE = 'http://openapi.qingtaoke.com'
 const URL_TAOBAO_DETAIL = 'https://h5api.m.taobao.com/h5/mtop.taobao.detail.getdetail/6.0/?data=%7B%22itemNumId%22%3A%22'
 const URL_TAOBAO_DESC = 'http://hws.m.taobao.com/d/modulet/v5/WItemMouldDesc.do?'
 const URL_TAOBAO_COUPON = 'https://pub.alimama.com/common/code/getAuctionCode.json?adzoneid=63092300043&siteid=230450350&scenes=1&auctionid='
-const URL_TAOBAO_SEARCH = 'http://pub.alimama.com/items/search.json?'
+const URL_TAOBAO_SEARCH = 'https://pub.alimama.com/items/search.json?'
 
 /***
 TEXT: 【三只松鼠_氧气吐司面包800g/整箱】夹心吐司口袋面包早餐多口味
@@ -111,7 +111,7 @@ app.get('/taobao/search', function (req, res, next) {
   request(options, function(error, response, body) {
     if (!error && response.statusCode == 200) {
       res.setHeader('Content-Type', 'application/json')
-      if (!JSON.parse(body).data.pageList || (typeof JSON.parse(body).data.pageList === "undefined") || JSON.parse(body).data.pageList == null || JSON.parse(body).data.pageList.length == 0) {
+      if (!JSON.parse(body).data || (typeof JSON.parse(body).data === "undefined") || JSON.parse(body).data.pageList == null || JSON.parse(body).data.pageList.length == 0) {
         res.send({'er_code': 10000, 'er_msg': '', 'data': {'total':0, 'list': []}})
         return
       }
